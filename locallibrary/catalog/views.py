5fb96f55b2ce6aaae6a23a356f3151f5c899311f
acferten <gowiththeirname@gmail.com>
2022-09-15 14:16:52 +0700

    Changed the Title and created the amount of Novels on the main page

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -5,8 +5,6 @@ from .models import Book, Author, BookInstance, Genre
 
 
 def index(request):
-    """View function for home page of site."""
-
     # Generate counts of some main objects
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
@@ -17,12 +15,14 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    num_novels = Genre.objects.filter(name='Novel').count()
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_novels': num_novels,
     }
-
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
