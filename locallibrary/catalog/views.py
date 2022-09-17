e9136907626102c5a30f0ccd5e2dbc3ec9521235
acferten <gowiththeirname@gmail.com>
2022-09-17 11:41:03 +0700

    Challenge yourself in the eight module is 90% done

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Book, Author, BookInstance, Genre
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required, permission_required
 
 
 def index(request):
@@ -19,6 +20,7 @@ def index(request):
 
     num_visits = request.session.get('num_visits', 0)
     request.session['num_visits'] = num_visits + 1
+
 
     context = {
         'num_books': num_books,
@@ -53,13 +55,23 @@ class AuthorDetailView(generic.DetailView):
     model = Author
     template_name = 'author_detail.html'
 
-class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
     """Generic class-based view listing books on loan to current user."""
     model = BookInstance
-    template_name ='bookinstance_list_borrowed_user.html'
+    template_name = 'bookinstance_list_borrowed_user.html'
     paginate_by = 10
 
     def get_queryset(self):
         return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
 
 
+class AllLoanedBooksListView(LoginRequiredMixin, generic.ListView):
+    permission_required = 'catalog.can_mark_returned'
+    model = BookInstance
+    template_name = 'all_borrowed_books.html'
+    paginate_by = 10
+
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(status__exact='o').order_by('due_back')
